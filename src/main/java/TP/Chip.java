132e20d3d1730b8d818c917b5e320d756fd0eeb0
CCamposD <ccamposd@fi.uba.ar>
2024-11-11 14:23:44 -0300

    Principales 'atributos' que contiene puntaje, quizás un modificar Chip/Multiplicador no sea necesario, ya que se puede reutilizar el 'sumar'

--- src/main/java/TP/Chip.java
+++ src/main/java/TP/Chip.java
@@ -1,4 +1,21 @@
 package TP;
 
 public class Chip {
+
+    private int valor;
+
+    // Constructor
+    public Chip(int valor) {
+        this.valor = valor;
+    }
+
+    public Chip sumar(Chip otroChip) {
+        return new Chip(this.valor + otroChip.valorNumerico());
+    }
+
+    // Solo usar en Puntajes, si se usa afuera, se rompe pilar
+    // Recordar: Lo que pasa en Chip queda en Chip
+    public int valorNumerico() {
+        return this.valor;
+    }
 }
--- src/main/java/TP/Multiplicador.java
+++ src/main/java/TP/Multiplicador.java
@@ -1,4 +1,21 @@
 package TP;
 
 public class Multiplicador {
+
+    private float valor;
+
+    // Constructor
+    public Multiplicador(float valor) {
+        this.valor = valor;
+    }
+
+    public Multiplicador sumar(Multiplicador otroMultiplicador) {
+        return new Multiplicador(this.valor + otroMultiplicador.valorNumerico());
+    }
+
+    // Solo usar en Puntajes, si se usa afuera, se rompe pilar
+    // Recordar: Lo que pasa en Multiplicador queda en Multiplicador
+    public float valorNumerico() {
+        return this.valor;
+    }
 }
