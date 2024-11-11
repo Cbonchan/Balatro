6b296e7eeffdf23ca77be73876ee8dad82797624
CCamposD <ccamposd@fi.uba.ar>
2024-11-11 14:22:16 -0300

    Falta pulir el correcto funcionamiento desde las clases externas (Manos de poker)

---
+++ src/main/java/TP/Puntaje.java
@@ -0,0 +1,36 @@
+package TP;
+
+public class Puntaje {
+
+    private Chip chip;
+    private Multiplicador multiplicador;
+
+    // Constructor
+    public Puntaje(Chip chip) {
+        this(chip, new Multiplicador(1));
+    }
+
+    public Puntaje(Chip chip, Multiplicador multiplicador) {
+        this.chip = chip;
+        this.multiplicador = multiplicador;
+    }
+
+    // Métodos
+    public Puntaje aumentarValorChip(Chip nuevoChip){
+        // Suma el valor numérico del nuevo chip con el actual y devuelve un nuevo Puntaje
+        Chip nuevoChipTotal = this.chip.sumar(nuevoChip);
+        return new Puntaje(nuevoChipTotal, this.multiplicador); // Retorna un nuevo Puntaje con la suma
+    }
+
+    public Puntaje sumarMultiplicador(Multiplicador nuevoMultiplicador){
+
+        Multiplicador nuevoMultiplicadorTotal = this.multiplicador.sumar(nuevoMultiplicador);
+        return new Puntaje(this.chip, nuevoMultiplicadorTotal);
+    }
+
+    public Puntaje sumarChipCon(Puntaje otroPuntaje){
+        Chip chipTotal = this.chip.sumar(otroPuntaje.chip);
+        return new Puntaje(chipTotal, this.multiplicador); // Mantiene el mismo multiplicador
+    }
+
+}
---
+++ src/main/java/TP/Puntuable.java
@@ -0,0 +1,6 @@
+package TP;
+
+// Esto lo implementrán las manos de poker, tengo dudas si ahora cada carta del naipe (mazo) o solo los Chips
+public interface Puntuable {
+    Puntaje calcularPuntaje();
+}
