c4456491478e31afa6c9283daeae2cfb497adbb2
CCamposD <ccamposd@fi.uba.ar>
2024-11-15 07:09:07 -0300

    Carpeta con el objetivo de ser una rama, esto nos ayudará a no romper encapsulamiento y a la vez, una señal de si estamos rompiendo o no, al ir tan lejos para tener un dato primitivo

--- src/main/java/Modelo/Juego/Tablero.java
+++ src/main/java/Modelo/Juego/Tablero.java
@@ -1,18 +1,22 @@
 package Modelo.Juego;
+
+// Importaciones
 import Modelo.Jugador.Jugador;
 import Modelo.Jugador.Mano;
-import Modelo.Tarot;
+import Modelo.SistemaCartas.Tarot.*;
 
 import java.util.List;
 
 public class Tablero {
+    // Atribnutos
     private List<Tarot> tarots;
     private Jugador jugador;
     //private List<jockers> jockers;
 
+    // Constructor
     public int jugarMano(Mano mano) {
         return mano.calcularPuntaje();
     }
 
-
+    //Métodos
 }
