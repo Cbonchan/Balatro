32d10b0bc476362d9b6a9b2cae6e83a4d842daf0
CCamposD <ccamposd@fi.uba.ar>
2024-11-15 07:06:42 -0300

    En lo personal debe haber una clase Carta, Carta son Poker, Tarot y Joker, CartaPoker, el nombre ya es una señal (Creo yo)

--- src/main/java/Modelo/SistemaCartas/Poker/CartaPoker.java
+++ src/main/java/Modelo/SistemaCartas/Poker/CartaPoker.java
@@ -1,22 +1,26 @@
 package Modelo.SistemaCartas.Poker;
 
-import Modelo.Figura;
-import Modelo.Palo;
+// Importaciones
+import Modelo.SistemaCartas.Poker.Palo.Palo;
+import Modelo.SistemaCartas.Poker.Figura.Figura;
 
 public class CartaPoker {
+    // Atributos
     private final Palo palo;
     private final Figura figura;
     private int puntaje;
     private int multiplicador;
 
+    // Constructor
     public CartaPoker(Palo palo, Figura figura) {
         this.palo = palo;
         this.figura = figura;
+        this.multiplicador = 0;
 
         puntaje = figura.devolverPuntaje();
-        this.multiplicador = 0;
     }
 
+    // Getters y Setters
     public void setPuntaje(int puntaje){
         this.puntaje = puntaje;
     }
@@ -24,28 +28,27 @@ public class CartaPoker {
     public void setMultiplicador(int multiplicador) {
         this.multiplicador = multiplicador;
     }
-    //------------------------------------------------------------------------
-    public int sumarValorAgregadoMultiplicador(int multiplicadorRecibido){
-
-        return (multiplicadorRecibido + multiplicador);
-    }
-    //-----------------------------------------------------------------------
-    public int sumarValorAgregadoPuntuacion(int puntajeRecibido){
-
-        return (puntajeRecibido + puntaje);
-    }
-
-    public boolean esFiguraIgualA(Figura figura){
-
-        return (this.figura.getNombre()==(figura.getNombre()));
-    }
-
-    // :'v
-    public Figura getFigura(){
-        return this.figura;
-    }
 
     public Palo getPalo() {
         return this.palo;
     }
+
+    public Figura getFigura(){
+        return this.figura;
+    }
+
+
+    // Métodos
+    public int sumarValorAgregadoMultiplicador(int multiplicadorRecibido){
+        return (multiplicadorRecibido + multiplicador);
+    }
+
+    public int sumarValorAgregadoPuntuacion(int puntajeRecibido){
+        return (puntajeRecibido + puntaje);
+    }
+
+    public boolean esFiguraIgualA(Figura figura){
+        return (this.figura.getNombre().equals(figura.getNombre()));
+    }
+
 }
