97d7a45507beff64740fed60806faa2af34b4688
CCamposD <ccamposd@fi.uba.ar>
2024-11-15 22:38:58 -0300

    Arreglo mínimo para Tarot y tener una noción como funcionará

--- src/main/java/Modelo/SistemaCartas/Tarot/Tarot.java
+++ src/main/java/Modelo/SistemaCartas/Tarot/Tarot.java
@@ -3,9 +3,28 @@ package Modelo.SistemaCartas.Tarot;
 // Importaciones
 import Modelo.SistemaCartas.Poker.CartaPoker;
 
-public abstract class Tarot {
+public class Tarot {
 
-    // Métodos
-    public abstract void usar(CartaPoker carta);
+    // Atributos
+    private final int chips;
+    private final int multiplicador;
+
+    // Constructor
+    public Tarot(int chips, int multiplicador) {
+        this.chips = chips;
+        this.multiplicador = multiplicador;
+    }
+
+    // Métodos públicos
+    public void cambiarPuntaje(CartaPoker carta) {
+        if (carta == null) {
+            throw new IllegalArgumentException("La carta no puede ser nula.");
+        }
+        carta.setPuntaje(chips);
+    }
+
+    public void cambiarMultiplicador(CartaPoker carta) {
+        carta.setMultiplicador(multiplicador);
+    }
+
 }
-
