760640ff3c7c55a6482560dada08af9467d5a6d7
Cbonchan <basterrasebastian@gmail.com>
2024-11-21 11:43:38 -0300

    agrego dependencias

--- src/main/test/Modelo/Unitarios/TestDescarte.java
+++ src/main/test/Modelo/Unitarios/TestDescarte.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 import  static  org.junit.jupiter.api.Assertions.*;
 
-public class DescarteTest {
+public class TestDescarte {
 
     @Test
     public void test01descartoCartasYelJugadorTieneMenos() {
