600702c95fce41e273bf61bddc6cba38a747098e
Cbonchan <basterrasebastian@gmail.com>
2024-11-23 17:04:35 -0300

    nuevos cambios en descarte

--- src/main/test/Modelo/Unitarios/TestCartaPoker.java
+++ src/main/test/Modelo/Unitarios/TestCartaPoker.java
@@ -1,8 +1,8 @@
-/*package Modelo.Unitarios;
+package Modelo.Unitarios;
 
 import Modelo.SistemaCartas.Poker.Palo.*;
 import Modelo.SistemaCartas.Poker.Figura.*;
-import Modelo.SistemaCartas.Poker.CartaPoker;
+import Modelo.SistemaCartas.Poker.Poker;
 
 import  org.junit.Test;
 import  static  org.junit.jupiter.api.Assertions.*;
@@ -10,11 +10,12 @@ import  static  org.junit.jupiter.api.Assertions.*;
 
 
 public class TestCartaPoker {
+    //!rehacer
 
-    @Test
-    public void test01CartaPokerCalculaPuntuacionAgregado() {
-        CartaPoker cartaPoker = new CartaPoker(new Diamante() ,new Reina());
-        CartaPoker cartaPoker2 = new CartaPoker(new Diamante() ,new As());
+    /*@Test
+    public void test01PokerCalculaPuntuacionAgregado() {
+        Poker cartaPoker = new Poker(new Diamante() ,new Reina());
+        Poker cartaPoker2 = new Poker(new Diamante() ,new As());
         int valorAgregado = 0;
         int valorEsperado = 21;
         valorAgregado = cartaPoker.sumarValorAgregadoPuntuacion(valorAgregado);
@@ -24,14 +25,14 @@ public class TestCartaPoker {
 
     }
     @Test
-    public void test02CartaPokerCalculaMultiplicadorAgregado() {
-        CartaPoker cartaPoker = new CartaPoker(new Diamante(), new Reina());
-        CartaPoker cartaPoker2 = new CartaPoker(new Diamante(), new As());
+    public void test02PokerCalculaMultiplicadorAgregado() {
+        Poker cartaPoker = new Poker(new Diamante(), new Reina());
+        Poker cartaPoker2 = new Poker(new Diamante(), new As());
         int valorAgregado = 0;
         int valorEsperado = 0;
         valorAgregado = cartaPoker.sumarValorAgregadoMultiplicador(valorAgregado);
         valorAgregado = cartaPoker2.sumarValorAgregadoMultiplicador(valorAgregado);
 
         assertEquals(valorEsperado, valorAgregado);
-    }
-}*/
+    }*/
+}
