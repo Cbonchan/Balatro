508fe5cb0a86cb824e69616dd7380146c2e18e53
Martin Limardo <68306628+Lemonchelo@users.noreply.github.com>
2024-11-30 16:02:03 -0300

    reparadas dependencias javaFX

---
+++ src/main/test/Modelo/ModeloController.java
@@ -0,0 +1,45 @@
+package Modelo;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.fxml.FXML;
+public class ModeloController{
+
+    @FXML
+    private Button boton_descartar;
+
+    @FXML
+    private Button boton_jugar;
+
+    @FXML
+    private Label nro_chips;
+
+    @FXML
+    private Label nro_mult;
+
+    @FXML
+    private Label nro_ronda;
+
+    @FXML
+    private Label puntaje;
+
+    @FXML
+    private Label puntos_a_superar;
+
+    @FXML
+    private Label tipo_jugada;
+
+    @FXML
+    void descartarMano(ActionEvent event) {
+
+    }
+
+    @FXML
+    void jugar(ActionEvent event) {
+
+    }
+
+}
+
