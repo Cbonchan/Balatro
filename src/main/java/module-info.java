75feea76bd068a2597d76a4f7eb30aab7fcd9dfc
CCamposD <ccamposd@fi.uba.ar>
2024-12-01 02:23:48 -0300

    Todos lo archivos utilizados van acá

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -8,11 +8,19 @@ module edu.fiuba.algo3 {
 
     // Arhivos del trabajo
     exports  Modelo.Juego;
-    exports Modelo.Usuario;
-    exports Modelo.SistemaCartas.Poker.Palo;
-    exports Modelo.SistemaCartas.Poker.Figura;
-    exports Modelo.SistemaCartas.Poker;
-    exports Modelo.SistemaCartas.Jugadas;
+
+    exports Modelo.SistemaCartas.Activables;
+    exports  Modelo.SistemaCartas.Activables.Joker;
+    exports  Modelo.SistemaCartas.Activables.SistemaDeEfecto;
     exports Modelo.SistemaCartas.Activables.Tarot;
+
+    exports Modelo.SistemaCartas.Jugadas;
+
+    exports Modelo.SistemaCartas.Poker.Figura;
+    exports Modelo.SistemaCartas.Poker.Palo;
+    exports Modelo.SistemaCartas.Poker;
+
     exports Modelo.SistemaPuntaje;
+
+    exports  Modelo.Usuario;
 }
