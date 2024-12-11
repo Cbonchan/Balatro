261be634002687634fd4ac50c6816c1250e80185
CCamposD <ccamposd@fi.uba.ar>
2024-12-11 02:23:28 -0300

    Se borran archivos de Test obsoletos, y se implementan los nuevos correspondientes

--- src/main/test/Modelo/TestUnitarios/TestUsuario/TestPilaDescarte.java
+++ src/main/test/Modelo/TestUnitarios/TestUsuario/TestPilaDescarte.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 import  static  org.junit.jupiter.api.Assertions.*;
 
-public class TestDescarte {
+public class TestPilaDescarte {
 
     @Test
     public void test01deshacerEleccionAumentaLasDisponibles(){
