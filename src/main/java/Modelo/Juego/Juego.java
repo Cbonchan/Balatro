ac87779c8c414241af586ef9e0f232e64039f962
Martin Limardo <68306628+Lemonchelo@users.noreply.github.com>
2024-12-13 18:07:58 -0300

    Update Juego.java

--- src/main/java/Modelo/Juego/Juego.java
+++ src/main/java/Modelo/Juego/Juego.java
@@ -1,6 +1,3 @@
-/*
-
-
 package Modelo.Juego;
 
 
@@ -21,15 +18,19 @@ public class Juego {
 
     // Atributos
     Jugador jugador;
-    Ronda ronda;
-    //List<Ronda> rondas;
+    int nroRonda;
+    Ronda rondaActual;
+    List<Ronda> rondas;
 
      public Juego(){
         this.jugador = new Jugador();
-        this.ronda = new Ronda(0,2,2,100, new Tienda(List.of(new Descarte("prueba","prueba",1,new Multiplicador(1),new SumarPuntaje())),new Carta(new Diamante(),new As())));
+        nroRonda = 1;
+        //aca deberia estar la lectura de los json, creo, sobretodo porque convendria almacenar la info de las rondas en inicializacion en lugar de
+        //obtenerla en tiempo de ejecucion
+        //this.ronda = new Ronda(0,2,2,100, new Tienda(List.of(new Descarte("prueba","prueba",1,new Multiplicador(1),new SumarPuntaje())),new Carta(new Diamante(),new As())));
     }
 
-/*
+
     public void comenzarJuego(){
         jugador
         // inicializamos rondas
@@ -38,6 +39,9 @@ public class Juego {
             ronda.comenzar();
             while(ronda.puedoContinuar() && !ronda.superoPuntaje(jugador)){
                 //! desconozco como funciona esto secuencialmente con javaFX como para completar
+
+                //!mucho de esto se hace de forma nativa en FX, aca tenemos que inicializar los componentes
+                //y facilitar funciones para avanzar, no nos encargamos del loop porque para eso es el controlador
             }
             if(!ronda.superoPuntaje(jugador)){
                 terminarJuego();
@@ -45,16 +49,22 @@ public class Juego {
             }
        }
 
-
-    }
-
-
     public void terminarJuego(){
         //TODO implementar
     }
 
-}
-*/
+    public void avanzarRonda() {
+        this.rondaActual=rondas.get(nroRonda-1);
+        nroRonda++;
+    }
+
+    }
+
+
+
+
+
+
 
 
 
